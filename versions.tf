fd0c9c3c74a5622658cc386efb8e232a0c0ca1ff
Balsir <jubo.stehlik@gmail.com>
2024-01-19 00:32:16 +0100

    fix: include helm parameters when not empty

--- versions.tf
+++ versions.tf
@@ -1,15 +1,14 @@
-
 terraform {
   required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.19.0"
+      version = ">= 4.35.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.11.0"
+      version = ">= 2.20.0"
     }
     helm = {
       source  = "hashicorp/helm"
